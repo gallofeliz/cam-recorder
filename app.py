f2d2fde0a1b5cd6c9d24840a4a08805467c7d9c6
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-02-18 20:03:47 +0100

    Oh yeah !

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import threading, sched, time, requests, os, re, glob, logging
 from datetime import datetime
 from pythonjsonlogger import jsonlogger
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO').upper())
 
 logHandler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter('%(levelname)%(message)')
@@ -14,26 +14,35 @@ def convert_to_seconds(duration):
     seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
     return int(duration[:-1]) * seconds_per_unit[duration[-1].lower()]
 
-records_config = [
-    {
-        'name': 'cam1-snapshot',
-        'type': 'snapshot',
-        'url': 'http://user:pass@192.168.1.50/api/v1/snap.cgi?chn=1',
-        'schedule': '5s',
-        'keepTime': '10s',
-        'pruneSchedule': '30s',
-        'fileFormat': '/tmp/records/cam1/{date}/{datetime}.jpg'
-    },
-    {
-        'name': 'cam2-snapshot',
-        'type': 'snapshot',
-        'url': 'http://user:pass@192.168.1.50/api/v1/snap.cgi?chn=1',
-        'schedule': '12s',
-        'keepTime': '1m',
-        'pruneSchedule': '30s',
-        'fileFormat': '/tmp/records/cam2/{date}/{datetime}.jpg'
-    }
-]
+def load_config():
+    records = []
+
+    env_records = {}
+
+    for k, v in os.environ.items():
+        if k[0:7] == 'RECORD_':
+            name, *rest = k[7:].split('_')
+            name = name.lower()
+            rest = '_'.join(rest)
+
+            if name not in env_records:
+                env_records[name] = {}
+
+            env_records[name][rest] = v
+
+    for name in env_records:
+        values = env_records[name]
+        records.append({
+            'name': name,
+            'type': values['TYPE'],
+            'url': values['URL'],
+            'schedule': values['SCHEDULE'],
+            'keepTime': values['KEEP_TIME'], # None to disable ? We will see ...
+            'pruneSchedule': values['PRUNE_SCHEDULE'],
+            'fileFormat': values['FILE_FORMAT']
+        })
+
+    return records
 
 def record(record_config):
     if record_config['type'] != 'snapshot':
@@ -59,9 +68,9 @@ def record(record_config):
             f = open(filename, "wb")
             f.write(response.content)
             f.close()
-            logging.info('Snaphot end', extra={'action': 'snapshot', 'status': 'success', 'record': record_config['name']})
+            logging.info('Snaphot end ; file {}'.format(filename), extra={'action': 'snapshot', 'status': 'success', 'record': record_config['name']})
         except Exception as e:
-            logging.exception('Snaphot start', extra={'action': 'snapshot', 'status': 'failure', 'record': record_config['name']})
+            logging.exception('Snaphot error', extra={'action': 'snapshot', 'status': 'failure', 'record': record_config['name']})
     do_snapshot()
     s.run()
 
@@ -75,11 +84,14 @@ def prune(record_config):
             glob_ = re.compile('{[^}]+}').sub('*', record_config['fileFormat'])
 
             max = time.time() - convert_to_seconds(record_config['keepTime'])
+            totalFile = 0
+            totalDir = 0
             directories = []
 
             for file in glob.iglob(glob_):
                 if os.stat(file).st_mtime < max:
                     os.remove(file)
+                    totalFile += 1
                     logging.debug('Deleted file ' + file, extra={'action': 'prune', 'status': 'running', 'record': record_config['name']})
                     dirname = os.path.dirname(file)
                     if dirname not in directories:
@@ -90,17 +102,19 @@ def prune(record_config):
                 # We can image take the no dynamic part of the path and check parents directories
                 if not os.listdir(directory):
                     os.rmdir(directory)
+                    totalDir += 1
                     logging.debug('Deleted directory ' + directory, extra={'action': 'prune', 'status': 'running', 'record': record_config['name']})
 
-            logging.info('Prune end', extra={'action': 'prune', 'status': 'success', 'record': record_config['name']})
+            logging.info('Prune end ; {} files deleted ; {} directories deleted'.format(totalFile, totalDir), extra={'action': 'prune', 'status': 'success', 'record': record_config['name']})
 
         except Exception as e:
-            logging.exception('Prune end', extra={'action': 'prune', 'status': 'failure', 'record': record_config['name']})
+            logging.exception('Prune error', extra={'action': 'prune', 'status': 'failure', 'record': record_config['name']})
 
     do_prune()
     s.run()
 
+records_config = load_config()
+
 for record_config in records_config:
     threading.Thread(target=record, args=(record_config,)).start()
     threading.Thread(target=prune, args=(record_config,)).start()
-
