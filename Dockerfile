1d7b186d247bcdfda728b6da966260c4c96ce284
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-03-13 17:39:59 +0100

    Add thumb support

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM python:alpine
 
 RUN pip install python-json-logger requests
 
+RUN apk add --update --no-cache --virtual .tmp gcc libc-dev linux-headers \
+    && apk add --no-cache jpeg-dev zlib-dev \
+    && pip install Pillow \
+    && apk del .tmp
+
 WORKDIR /app
 
 ADD app.py .
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import threading, sched, time, requests, os, re, glob, logging
 from datetime import datetime
 from pythonjsonlogger import jsonlogger
+from PIL import Image
 
 logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO').upper())
 
@@ -14,6 +15,21 @@ def convert_to_seconds(duration):
     seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
     return int(duration[:-1]) * seconds_per_unit[duration[-1].lower()]
 
+"""
+    {
+        name: 'cam1-snapshot',
+        type: 'snapshot',
+        url: 'http://user:pass@192.168.1.50/api/v1/snap.cgi?chn=1',
+        schedule: '30s',
+        keepTime: '15d',
+        pruneSchedule: '1d',
+        fileFormat: '/tmp/records/cam1/{date}/{datetime}.jpg',
+        thumbs: {
+            size: [768, 5000],
+            fileFormat: '/tmp/records/cam1/{date}/thumbs/{datetime}.jpg'
+        }
+    }
+"""
 def load_config():
     records = []
 
@@ -32,7 +48,7 @@ def load_config():
 
     for name in env_records:
         values = env_records[name]
-        records.append({
+        o = {
             'name': name,
             'type': values['TYPE'],
             'url': values['URL'],
@@ -40,7 +56,14 @@ def load_config():
             'keepTime': values['KEEP_TIME'], # None to disable ? We will see ...
             'pruneSchedule': values['PRUNE_SCHEDULE'],
             'fileFormat': values['FILE_FORMAT']
-        })
+        }
+        if 'THUMBS_SIZE' in values or 'THUMBS_FILE_FORMAT' in values:
+            o['thumbs'] = {
+                'size': list(map(lambda x: int(x) if x else 5000, values['THUMBS_SIZE'].split(','))),
+                'fileFormat': values['THUMBS_FILE_FORMAT']
+            }
+
+        records.append(o)
 
     return records
 
@@ -50,16 +73,31 @@ def record(record_config):
 
     s = sched.scheduler(time.time, time.sleep)
 
+    def do_thumb(filename, filenameParams):
+        try:
+            logging.info('Thumb start', extra={'action': 'snapshot-thumb', 'status': 'starting', 'record': record_config['name']})
+            im = Image.open(filename)
+            im.thumbnail(record_config['thumbs']['size'])
+            thumbFilename = record_config['thumbs']['fileFormat'].format(**filenameParams)
+            dirname = os.path.dirname(thumbFilename)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            im.save(thumbFilename, "JPEG")
+            logging.info('Thumb end ; file {}'.format(thumbFilename), extra={'action': 'snapshot-thumb', 'status': 'success', 'record': record_config['name']})
+        except Exception as e:
+            logging.exception('Thumb error', extra={'action': 'snapshot-thumb', 'status': 'failure', 'record': record_config['name']})
+
     def do_snapshot():
         s.enter(convert_to_seconds(record_config['schedule']), 1, do_snapshot)
         try:
             logging.info('Snaphot start', extra={'action': 'snapshot', 'status': 'starting', 'record': record_config['name']})
             now = datetime.now()
-            filename = record_config['fileFormat'].format(
-                date=now.strftime("%Y-%m-%d"),
-                time=now.strftime("%H-%M-%S"),
-                datetime=now.strftime("%Y-%m-%dT%H-%M-%S")
-            )
+            filenameParams = {
+                'date': now.strftime("%Y-%m-%d"),
+                'time': now.strftime("%H-%M-%S"),
+                'datetime': now.strftime("%Y-%m-%dT%H-%M-%S")
+            }
+            filename = record_config['fileFormat'].format(**filenameParams)
             dirname = os.path.dirname(filename)
             response = requests.get(record_config['url'])
             response.raise_for_status()
@@ -69,6 +107,8 @@ def record(record_config):
             f.write(response.content)
             f.close()
             logging.info('Snaphot end ; file {}'.format(filename), extra={'action': 'snapshot', 'status': 'success', 'record': record_config['name']})
+            if 'thumbs' in record_config:
+                do_thumb(filename, filenameParams)
         except Exception as e:
             logging.exception('Snaphot error', extra={'action': 'snapshot', 'status': 'failure', 'record': record_config['name']})
     do_snapshot()
@@ -81,23 +121,31 @@ def prune(record_config):
         s.enter(convert_to_seconds(record_config['pruneSchedule']), 1, do_prune)
         try:
             logging.info('Prune start', extra={'action': 'prune', 'status': 'starting', 'record': record_config['name']})
-            glob_ = re.compile('{[^}]+}').sub('*', record_config['fileFormat'])
+            globs_ = [
+                re.compile('{[^}]+}').sub('*', record_config['fileFormat'])
+            ]
+
+            if 'thumbs' in record_config:
+                globs_.append(
+                    re.compile('{[^}]+}').sub('*', record_config['thumbs']['fileFormat'])
+                )
 
             max = time.time() - convert_to_seconds(record_config['keepTime'])
             totalFile = 0
             totalDir = 0
             directories = []
 
-            for file in glob.iglob(glob_):
-                if os.stat(file).st_mtime < max:
-                    os.remove(file)
-                    totalFile += 1
-                    logging.debug('Deleted file ' + file, extra={'action': 'prune', 'status': 'running', 'record': record_config['name']})
-                    dirname = os.path.dirname(file)
-                    if dirname not in directories:
-                        directories.append(dirname)
+            for glob_ in globs_:
+                for file in glob.iglob(glob_):
+                    if os.stat(file).st_mtime < max:
+                        os.remove(file)
+                        totalFile += 1
+                        logging.debug('Deleted file ' + file, extra={'action': 'prune', 'status': 'running', 'record': record_config['name']})
+                        dirname = os.path.dirname(file)
+                        if dirname not in directories:
+                            directories.append(dirname)
 
-            for directory in directories:
+            for directory in reversed(sorted(directories, key=len)):
                 # I don't do parents directory but why not ...
                 # We can image take the no dynamic part of the path and check parents directories
                 if not os.listdir(directory):
